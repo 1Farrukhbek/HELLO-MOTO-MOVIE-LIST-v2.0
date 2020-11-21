03e170a324f2c356a806e7bc067cb60df831a45e
Farrukhbek <68499298+1Farrukhbek@users.noreply.github.com>
2020-11-21 23:54:27 +0500

    Add JS code and test rendering movie cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-// Create normalized Movie list 
-
+// CREATE NORMALIZED MOVIE ARRAY
 var normalizedMovieList = movies.map(function (movie, i) {
     return {
         id: i + 1,
@@ -11,9 +10,53 @@ var normalizedMovieList = movies.map(function (movie, i) {
         imdbRating: movie.imdb_rating,
         runtime: movie.runtime,
         language: movie.language,
+        youtubeImg: `http://i3.ytimg.com/vi/${movie.ytid}/sddefault.jpg`,
         youtubeId: `https://youtube.com/watch?v=${movie.ytid}`
     }
 });
 
+var miniTest = normalizedMovieList.slice(0, 100);
+
 // SELECT ITEMS FROM HTML 
+var elMovieList = $_('.js-movie-list');
+var elMovieCardTemplate = $_('#movie-template').content;
+
+// CREATE MOVIE ELEMENTS FROM TEMPLATE
+var createMovieElement = function (movie) {
+    var templateMovieElement = elMovieCardTemplate.cloneNode(true);
+    
+    $_('.js-movie-img', templateMovieElement).src = movie.youtubeImg;
+    $_('.js-movie-front-title', templateMovieElement).textContent = movie.title;
+    $_('.js-movie-back-title', templateMovieElement).textContent = movie.title;
+    $_('.js-movie-summary', templateMovieElement).textContent = movie.summary;
+    $_('.js-youtube-link', templateMovieElement).href = movie.youtubeId;
+    
+    return templateMovieElement;
+}
+
+var renderMovie = function (movies) {
+    elMovieList.innerHTML = '';
+    
+    var moviesFragment = document.createDocumentFragment();
+    
+    movies.forEach(function (movie) {
+        moviesFragment.appendChild(createMovieElement(movie));
+    });
+    
+    elMoviesList.appendChild(moviesFragment);
+};
+
+var renderMovieElements = function (movies) {
+    elMovieList.innerHTML = '';
+    
+    var elMoviesWrapperFragment = document.createDocumentFragment();
+    
+    movies.forEach(function (movie) {
+        elMoviesWrapperFragment.appendChild(createMovieElement(movie));
+    });
+    
+    elMovieList.appendChild(elMoviesWrapperFragment);
+};
+
+renderMovieElements(miniTest)
 
